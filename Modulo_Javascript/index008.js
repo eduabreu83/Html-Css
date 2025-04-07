a38dd9f5de09906ed69c54bc9cc63a5505a37f35
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-07 20:13:56 -0300

    at

---
+++ Modulo_Javascript/index008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do sei carro é ${vel}`)
+if (vel > 50){
+    console.log(`Voce ultrapassou a Velocidade, MULTADO`)
+}
+console.log(`Dirija sempre usando  cinto de segurança`)
