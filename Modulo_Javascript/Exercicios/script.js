129465e7cd18eec2e8762c5cebfc0b645a53d3f5
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-08 22:46:36 -0300

    at

--- Modulo_Javascript/Exercicios/script.js
+++ Modulo_Javascript/Exercicios/script.js
@@ -7,9 +7,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12 ) {
         img.src = 'dia.png'
+        document.body.style.background = '#blue'
     } else if (hora >= 12 && hora < 18){
         img.src = 'tarde.png'
+         document.body.style.background = '#e2cd9f'
     } else{
         img.src = 'noite.png'
+         document.body.style.background = 'gray'
     }
 }
