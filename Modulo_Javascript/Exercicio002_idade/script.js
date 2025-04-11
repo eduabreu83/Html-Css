f398a7c8e6e9f1557a0c3333e3a8ad6cc33930d1
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-11 11:25:45 -0300

    at

--- Modulo_Javascript/Exercicio002_idade/script.js
+++ Modulo_Javascript/Exercicio002_idade/script.js
@@ -2,10 +2,19 @@ function verificar(){
 var data = new Date()
 var ano = data.getFullYear()
 var fano = document.getElementById('txtano')
-var res = window.document.getElementById('div#res')
-if (fano.value.length == 0 || Number(fano.value) > ano){
+var res = document.querySelector('div#res')
+if (fano.value.length == 0 || Number (fano.value)> ano){
     window.alert(`[ERRO] Verifique os dados e tente novamente`)
-} else{
+} else {
     var fsex = document.getElementById('radsex')
+    var idade = ano - Number(fano.value)
+    //res.innerHTML = `Idade Calculada: ${idade}`
+    var genero = ''
+    if (fsex[0].checked){
+        genero = 'Homen'
+    } else if (fsex[1].checked){
+        genero = 'Mulher'
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
