4ddad1dd44a40cc05c1fef8738d68f23e94a5944
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-14 17:21:34 -0300

    at

--- Modulo_Javascript/Exercicio003_Contador/script.js
+++ Modulo_Javascript/Exercicio003_Contador/script.js
@@ -5,15 +5,31 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO - Favor inserir os dados! ')
+        //window.alert('[ERRO - Favor inserir os dados! ')
+        res.innerHTML = `Impossivel contar!`
     } else {
-        res.innerHTML = 'Contanto....'
+        res.innerHTML = 'Contanto.... <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c+= p){
-            res.innerHTML += `${c} `
+        
+        if (p <= 0){
+            window.alert(`Passo Invalido Considerendo passo 1`)
+                p = 1
         }
+        if (i <f ){
+            //contagem crescente
+            for(let c = i; c <= f; c+= p){
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        
+        } else {
+            //contagem regressiva
+            for (let c = i; c>= f; c -= p ){
+                res.innerHTML += `${c} \u{1f449}`
+
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
---
+++ Modulo_Javascript/Exercicio004_Tabuada/script.js
@@ -0,0 +1,10 @@
+function tabuada() {
+    let  num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        window.alert('Por favor digite um numero!')
+    } else {
+        let n = Number(num.value)
+    }
+    
+}
