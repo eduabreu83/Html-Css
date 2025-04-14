cd4b369390d79906ac1a2616ae8cbc8f37cb1f25
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-14 14:53:48 -0300

    Update script.js

--- Modulo_Javascript/Exercicio003_Contador/script.js
+++ Modulo_Javascript/Exercicio003_Contador/script.js
@@ -1,7 +1,7 @@
 function contar(){
-    let ini = document.getElementById(`txti`)
-    let fim = document.getElementById(`txtf`)
-    let passo = document.getElementById(`txtp`)
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
 
     if (ini.ariaValueMax.length == 0 || fim.ariaValueMax.length == 0 || passo.ariaValueMax.length == 0){
         window.alert('[ERRO - Favor inserir os dados! ')
