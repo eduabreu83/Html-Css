28f312c76873552cd733c18102315bef98a5a051
eduabreu83 <eduardoabreu83@gmail.com>
2025-04-16 10:22:38 -0300

    Create ambiente.js

---
+++ Modulo_Javascript/Funcoes/ambiente.js
@@ -0,0 +1,15 @@
+let num = [1,2,3,4,5,6,7,8,]
+num.push(9) //adicionar um novo elemento na lista do array
+num.sort()
+console.log(`Nosso vetor é o ${num}`)
+console.log(`o Vetor tem ${num.length} posições`)
+
+for (let pos=0;pos<num.length;pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+for(let pos in num){
+    console.log(`A posição ${pos} tem o Valor ${num[pos]}`)
+}
+
+console.log(num.indexOf(4))
